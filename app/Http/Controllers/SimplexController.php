9c47b74cbd5fff54c805220afe017703b76352d0
Daniel Damasceno <dandamasceno04@gmail.com>
2025-05-09 13:24:26 -0300

    visualizando os dados do request

--- app/Http/Controllers/SimplexController.php
+++ app/Http/Controllers/SimplexController.php
@@ -8,6 +8,33 @@ class SimplexController extends Controller
 {
     // PROCESSA OS DADOS E CHAMA AS SERVICES PARA FORMA PADRÃO E RESOLUÇÃO DO ALGORITMO
     public function processar(Request $request) {
-        dd($request);
+
+        // dd($request->all());
+
+        // DADOS DO REQUEST
+        $tipo = $request->input('tipo'); // string
+        $variaveis = (int) $request->input('variaveis'); // int
+        $restricoes = (int) $request->input('restricoes'); // int 
+        $z = $request->input('z'); // array
+        $restricoesData = $request->input('restricoes'); // array de array
+
+
+        echo $tipo . "<br>"; // max ou min
+        echo $variaveis . "<br>"; // quantidade de variaveis
+        echo $restricoes . "<br>"; // quantidade de restrições
+        echo "<br>";
+        foreach ($z as $zElement) {
+            // imprime cada elemento da linha Z
+            echo $zElement . " ";
+        }
+        echo "<br>";
+        foreach ($restricoesData as $restricao) {
+            foreach ($restricao as $data) {
+                // imprime os dados de cada restrição
+                echo $data . " ";
+            }
+            echo "<br>";
+        }
+
     }
 }
