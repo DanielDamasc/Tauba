381337826abfa1b4fc94a294567c505e80c220f9
Daniel Damasceno <dandamasceno04@gmail.com>
2025-05-10 18:38:21 -0300

    forma aumentada desordenada

--- app/Services/FormaAumentadaService.php
+++ app/Services/FormaAumentadaService.php
@@ -36,34 +36,90 @@ class FormaAumentadaService
             echo "<br>";
         }
 
+        // DADOS COM Z E RESTRIÇÕES JUNTOS
+        $restricoesData[0] = $z;
+        ksort($restricoesData);
 
+        // TESTES DE COMO EU VOU ADICIONAR AS VARIÁVEIS DE FOLGA
+        // $variavelFolga = '1';
+        // $variavelArtificial = '1';
+        // $variavelExcesso = '-1';
+        // $contVar = 0;
+        // $arrTeste = array_map(function($linhasProb) use ($variavelFolga, $variavelArtificial, $variavelExcesso, $contVar, $variaveis) {
+        //     // fazer condição para dar push das variáveis conforme a restrição ...
+        //     if (isset($linhasProb["sinal"])) {
+        //         switch ($linhasProb["sinal"]) {
+        //             case "<=":
+        //                 for ($i = 0; $i < $contVar; $i++) {
+        //                     array_push($linhasProb, '0');
+        //                 }
+        //                 $contVar++;
+        //                 array_push($linhasProb, $variavelFolga);
+        //                 break;
 
-        // GUARDA OS SINAIS E OS TERMOS EM ARRAYS
-        $sinais = [];
-        $termos = [];
-        foreach ($restricoesData as $restricao) {
-            foreach ($restricao as $key => $value) {
-                if ($key == "sinal") {
-                    $sinais[] = $value;
-                }
-                if ($key == "rhs") {
-                    $termos[] = $value;
+        //             case "=":
+        //                 for ($i = 0; $i < $contVar; $i++) {
+        //                     array_push($linhasProb, '0');
+        //                 }
+        //                 $contVar++;
+        //                 array_push($linhasProb, $variavelArtificial);
+        //                 break;
+
+        //             case ">=":
+        //                 for ($i = 0; $i < $contVar; $i++) {
+        //                     array_push($linhasProb, '0');
+        //                 }
+        //                 $contVar += 2;
+        //                 array_push($linhasProb, $variavelExcesso, $variavelArtificial);
+        //                 break;
+        //         }
+        //     }
+        //    return [$linhasProb, $contVar];
+        // }, $restricoesData);
+
+        $variavelFolga = '1';
+        $variavelExcesso = '-1';
+        $variavelArtificial = '1';
+        $contVar = 0;
+        $arrResultado = [];
+        $numVar = $variaveis + 1;
+
+        foreach ($restricoesData as $index => $restricao) {
+            $linha = $restricao; // cópia da linha
+
+            // adiciona zeros correspondentes às variáveis já adicionadas em restrições anteriores
+            for ($i = $numVar; $i < $contVar + $numVar; $i++) {
+                $linha["$i"] = '0';
+            }
+
+            // adicionar variáveis conforme o sinal da restrição
+            if (isset($restricao["sinal"])) {
+                switch ($restricao["sinal"]) {
+                    case "<=":
+                        $num = $contVar + $numVar;
+                        $linha["$num"] = $variavelFolga;
+                        $contVar++;
+                        break;
+
+                    case "=":
+                        $num = $contVar + $numVar;
+                        $linha["$num"] = $variavelArtificial;
+                        $contVar++;
+                        break;
+
+                    case ">=":
+                        $num = $contVar + $numVar;
+                        $linha["$num"] = $variavelExcesso;
+                        $contVar++;
+                        $num = $contVar + $numVar;
+                        $linha["$num"] = $variavelArtificial;
+                        $contVar++;
+                        break;
                 }
             }
+
+            $arrResultado[] = $linha;
         }
-
-        // ARRAY DAS RESTRIÇÕES SEM SINAIS E TERMOS
-        $coeficientes = [];
-        foreach ($restricoesData as $restricao) {
-            unset($restricao["sinal"]);
-            unset($restricao["rhs"]);
-            $coeficientes[] = $restricao;
-        }
-
-        // MONTANDO O PROBLEMA
-
-        dd($coeficientes);
-        
 
         return $request;
     }
